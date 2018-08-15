9f2b739ef514c23912f4c58eaf74ca330043bb5f
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2018-08-14 23:23:27 -0300

    add support to es6

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 9000
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['babel-preset-env', 'babel-preset-stage-0']
+              }
+            }
+          }
+        ]
+      }
 }
